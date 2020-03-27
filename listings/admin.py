df84571174c5f54775caf0ee24ae19244d144b58
Emmanuel A. Valdez <e.avila.donata@gmail.com>
2020-03-27 13:42:38 -0700

    admin css and display data customization

--- listings/admin.py
+++ listings/admin.py
@@ -3,5 +3,21 @@ from django.contrib import admin
 from listings.models import Listing
 
 
-admin.site.register(Listing)
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "is_published", "price", "list_date", "realtor")
+    list_display_links = ("id", "title")
+    list_filter = ("realtor",)
+    list_editable = ("is_published",)
+    list_per_page = 25
+    search_fields = (
+        "title",
+        "description",
+        "address",
+        "city",
+        "state",
+        "zipcode",
+        "price",
+    )
 
+
+admin.site.register(Listing, ListingAdmin)
