abe33c021e2fab6129ae5751bc22c975848bc899
Emmanuel A. Valdez <e.avila.donata@gmail.com>
2020-03-27 15:34:01 -0700

    pull data from listings model and display listings in template

--- listings/urls.py
+++ listings/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from listings import views
 
 urlpatterns = [
-    path('', views.index, name='listings'),
-    path('<int:listing_id>', views.listing, name='listing'),
-    path('search/', views.search, name='search'),
+    path("", views.index, name="listings"),
+    path("<int:listing_id>", views.listing, name="listing"),
+    path("search/", views.search, name="search"),
 ]
