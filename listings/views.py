5a1487cb93076cdc2db9b256c6ef2ec80245b9c6
Emmanuel A. Valdez <e.avila.donata@gmail.com>
2020-03-31 15:07:03 -0700

    Added search form option functionality

--- listings/views.py
+++ listings/views.py
@@ -1,6 +1,7 @@
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.shortcuts import get_object_or_404, render
 
+from .choices import price_choices, bedroom_choices, state_choices
 from .models import Listing
 
 
@@ -26,4 +27,10 @@ def listing(request, listing_id):
 
 
 def search(request):
-    return render(request, "listings/search.html")
+
+    context = {
+        'state_choices': state_choices,
+        'bedroom_choices': bedroom_choices,
+        'price_choices': price_choices,
+    }
+    return render(request, "listings/search.html", context)
