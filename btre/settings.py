b07bd5fcf5336a776f15342e3ab5bfe052a6720e
Emmanuel A. Valdez <e.avila.donata@gmail.com>
2020-04-06 17:32:02 -0600

    inquiry check and send email

--- btre/settings.py
+++ btre/settings.py
@@ -140,3 +140,11 @@ from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.ERROR: 'danger',
 }
+
+# Email Config
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER='sleazye@live.com.mx'
+EMAIL_HOST_PASSWORD=''
+EMAIL_USE_TLS=True
